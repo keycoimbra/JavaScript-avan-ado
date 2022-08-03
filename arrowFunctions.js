dac80b1ba31b505f366d5ac0ba30bfd1d7a84970
Keyla Coimbra <keyla_thay@hotmail.com>
2022-08-03 18:55:09 -0300

    estudando arrowFunctions

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -4,5 +4,34 @@ são implementações de funções continuamente utilizadas no dia a dia.
 -> São para adicionar valor, remover valor, percorrer a lista, 
 juntar listas, fazer filtros e entre outros.
 
-1. Add valor
+1. Add valor: Para o começo e o final, há funções próprias para isso,
+são elas unshift e push.*/
+//Add final
+let linguagens = ["Javascript", "PHP", "Python"];
+linguagens.push("Java");
+console.log(linguagens);
 
+console.log("__________________");
+console.log("add valor inicio");
+
+//Add inicio
+linguagens.unshift("Cobol");
+console.log(linguagens); 
+
+//Splice - para remover ou add no meio, nesse caso eu add na posição 2,
+//que é depois de javascript
+
+console.log("__________________");
+console.log("add valor ao meio");
+
+let posicaoDoRuby = 2;
+linguagens.splice(posicaoDoRuby, 0, "Ruby");
+console.log(linguagens);
+
+//ForEach - percorre a lista sem fazer alterações
+console.log("__________________");
+console.log("ForEach");
+
+linguagens.forEach(linguagem => { 
+    console.log(`Linguagem: ${linguagem}`);
+  });
